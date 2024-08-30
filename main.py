484a2dda3fbb328c6e5b6d5cf330a26aad7f5229
Ashwaq <almalkiashwaq0@gmail.com>
2024-08-30 17:56:07 +0300

    Update

--- main.py
+++ main.py
@@ -2,47 +2,39 @@ from fastapi import FastAPI, HTTPException
 import joblib
 from pydantic import BaseModel
 
-
 app = FastAPI()
- # GET request
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to Tuwaiq Academy"}
+def root():
+    return "Welcome To Tuwaiq Academy"
 
 
-
-
-app = FastAPI()
-model = joblib.load("knn_model.joblib")
+model = joblib.load("kmeans_model.joblib")
+scaler = joblib.load("scaler_modell.joblib")
 
 class InputFeatures(BaseModel):
     appearance: int
     goals: float
-    assists: float
-    goals_conceded: float
-    minutes_played: int
-    days_injured: int
-    games_injured: int
     award: int
-    highest_value: int
-    position_Goalkeeper: bool
+    height: float
 
 def preprocessing(input_features: InputFeatures):
-    return {
+    dict_f =  {
         'appearance': input_features.appearance,
         'goals': input_features.goals,
-        'assists': input_features.assists,
-        'goals_conceded': input_features.goals_conceded,
-        'minutes_played': input_features.minutes_played,
-        'days_injured': input_features.days_injured,
-        'games_injured': input_features.games_injured,
         'award': input_features.award,
-        'highest_value': input_features.highest_value,
-        'position_Goalkeeper': input_features.position_Goalkeeper
+        'height': input_features.height,
     }
+     
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+    
+    scaled_features = scaler.transform([list(dict_f.values
+ ())])
+
+    return scaled_features
 
 @app.post("/predict")
 async def predict(input_features: InputFeatures):
     data = preprocessing(input_features)
-    y_pred = model.predict([list(data.values())])  # Ensure data is in the correct format
-    return {"prediction": y_pred.tolist()[0]}
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
+
